f6180c757c2efe062082e32ce6cae53ad77f5f08
Elba Cristina Rodriguez Perdomo <elbacristinarodriguezperdomo@gmail.com>
2021-01-31 02:08:03 -0600

    campos nuevos

--- vscode/main.js
+++ vscode/main.js
@@ -1,30 +1,32 @@
-var miDBProductos = openDatabase('dbProductos','1.0','Aplicacion de Productos',5*1024*1024);
+var miDBAlumnos = openDatabase('dbAlumnos','1.0','Aplicacion de Añumnos',5*1024*1024);
 window.id = 0;
-if(!miDBProductos){
+if(!miDBAlumnos){
     alert("Elnavegador no soporta Web SQL");
 }
 var appVue = new Vue({
-    el:'#appProductos',
+    el:'#appRegistroAlumnos',
     data:{
         producto:{
-            idProducto  : 0,
-            codigo      : '',
-            descripcion : '',
-            precio      : '',
+            idAlumno  : 0,
+            nombre      : '',
+            edad        : '',
+            sexo        : '',
+            grado       : '',
+            responsable : '',
             img         : '/images/No-image-available.png',
             img2        : '/images/No-image-available.png'
         },
-        productos:[]
+        alumno:[]
     },
     methods:{
-        guardarProducto(){
+        guardarAlumno(){
             /**
              * BD Web SQL
              */
-            miDBProductos.transaction(tran=>{
-                tran.executeSql('INSERT INTO productos(idProducto,codigo,descripcion,precio,img) VALUES(?,?,?,?,?) ',
-                    [++id,this.producto.codigo,this.producto.descripcion,this.producto.precio, this.producto.img]);
-                this.obtenerProductos();
+            miDBAlumnos.transaction(tran=>{
+                tran.executeSql('INSERT INTO productos(idAlumno,nombre,edad,sexo,grado,responsable,img) VALUES(?,?,?,?,?,?,?,?) ',
+                    [++id,this.alumno.nombre,this.alumno.edad,this.alumno.sexo, this.alumno.grado, this.alumno.responsable, this.alumno.img]);
+                this.obtenerAlumnos();
                 this.limpiar();
             }, err=>{
                 console.log( err );
@@ -36,31 +38,33 @@ var appVue = new Vue({
             this.producto.img = rutaTemp;
             //IMG2
             /*rutaTemp = URL.createObjectURL(e.target.files[1]);
-            this.producto.img2 = rutaTemp;*/
+            this.alumno.img2 = rutaTemp;*/
         },
-        obtenerProductos(){
-            miDBProductos.transaction(tran=>{
-                tran.executeSql('SELECT * FROM productos',[],(index,data)=>{
-                    this.productos = data.rows;
+        obtenerAlumnos(){
+            miDBAlumnos.transaction(tran=>{
+                tran.executeSql('SELECT * FROM alumnos',[],(index,data)=>{
+                    this.alumnos = data.rows;
                     id=data.rows.length;
                 });
             }, err=>{
                 console.log( err );
             });
         },
-        mostrarProducto(pro){
-            this.producto = pro;
+        mostrarAlumno(pro){
+            this.alumno = pro;
         },
         limpiar(){
-            this.producto.codigo='';
-            this.producto.descripcion='';
-            this.producto.precio='';
-            this.producto.img='';
+            this.alumno.nombre='';
+            this.alumno.edad='';
+            this.alumno.sexo='';
+            this.alumno.grado='';
+            this.alumno.responsable='';
+            this.alumno.img='';
         }
     },
     created(){
-        miDBProductos.transaction(tran=>{
-            tran.executeSql('CREATE TABLE IF NOT EXISTS productos(idProducto int PRIMARY KEY NOT NULL, codigo varchar(10), descripcion varchar(65), precio decimal(4,2),img varchar(100))');
+        miDBAlumnos.transaction(tran=>{
+            tran.executeSql('CREATE TABLE IF NOT EXISTS productos(idAlumno int PRIMARY KEY NOT NULL, codigo varchar(10), descripcion varchar(65), precio decimal(4,2),img varchar(100))');
         }, err=>{
             console.log( err );
         });
